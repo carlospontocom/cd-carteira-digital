6d5c61ddca4730b56521d759063dcc1000527f55
frontendcnn@gmail.com <frontendcnn@gmail.com>
2026-08-21 01:12:29 -0300

    feat:adicionar e excluir

--- src/components/CarteiraDigital.vue
+++ src/components/CarteiraDigital.vue
@@ -1,4 +1,31 @@
-<script setup></script>
+<script setup>
+  import '../styles/carteira.css';
+  import { ref } from 'vue';
+  const valor = ref('');
+  const descricao = ref('');
+  const tipo = ref('');
+
+  const dadosContas = ref([]);
+
+  const validarCampos = () =>{
+    if(valor.value.trim()==="" || descricao.value.trim()===""){
+      alert("Campos obrigatórios");
+      return;
+    }
+     dadosContas.value.push({
+      id:Date.now(),
+      valor:valor.value,
+      descricao:descricao.value,
+      tipo: tipo.value
+     })
+  }
+
+
+  const remover=(id)=>{
+     dadosContas.value = dadosContas.value.filter(item => item.id !== id);
+  }
+
+</script>
 
 <template>
   <section>
@@ -11,116 +38,40 @@
 
       <div class="header-saldo">
         <div class="the-saldo">
-          <span>Saldo</span>
+          <span class="title-saldo">Saldo</span>
           <p>R$ 1520,00</p>
         </div>
         <div class="the-gastos">
-          <span>Gastos</span>
+          <span class="title-gastos">Gastos</span>
           <p>R$ 20,00</p>
         </div>
       </div>
 
       <div class="bar-add">
-        <form class="form">
-          <input type="text" placeholder="R$">
-          <input type="text" placeholder="Descrição">
+        <form class="form" @submit.prevent="validarCampos">
+          <input type="text" placeholder="R$" v-model="valor">
+          <input type="text" placeholder="Descrição" v-model="descricao">
+          <select class="tipo" v-model="tipo">
+            <option value="gastos">Gastos</option>
+            <option value="Renda">Renda</option>
+          </select>
           <button type="submit">Adicionar</button>
         </form>
       </div>
 
       <ul class="lista-contas">
-        <li>
-          <h3>R$ 20,00</h3>
-          <div>
-            <h4>Arroz</h4>
-            <span>alimentação</span>
-          </div>
-          <button>X</button>
 
-        </li>
 
-        <li>
-          <h3>R$ 1500,00</h3>
+        <li v-for="itemLista in dadosContas" :key="itemLista.id">
+          <h3>R$ {{ itemLista.valor }}</h3>
           <div>
-            <h4>Notebook</h4>
-            <span>compras</span>
+            <h4>{{itemLista.descricao}}</h4>
+            <span>{{ itemLista.tipo}}</span>
           </div>
-          <button>X</button>
-        </li>
+          <button @click="remover(itemLista.id)">X</button>
+        </li> 
       </ul>
 
     </div>
   </section>
 </template>
-
-<style scoped>
-.container {
-  padding: 45px;
-  max-width: 450px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-}
-
-.header-carteira span {
-  left: 90%;
-  display: block;
-  letter-spacing: 1.4px;
-  color: #00000040;
-}
-
-.header-saldo{
-  display: flex;
-  gap:50px;
-}
-
-.the-saldo, .the-gastos{
-  border:1px solid #ccc;
-  padding: .5rem;
-}
-
-.bar-add {
-  margin: 1.5rem 0 0 0;
-}
-
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.form input {
-  padding: 10px 5px;
-}
-
-.form button {
-  background-color: seagreen;
-  color: white;
-  border: 0;
-  border-radius: 5px;
-  padding: 10px;
-}
-
-.lista-contas li{
-  display: grid;
-  grid-template-columns: 150px auto 40px;
-  gap:10px;
-  align-items: center;
-  padding:10px 0;
-  border-bottom: 1px saddlebrown solid;
-}
-
-.lista-contas li span{
-  font-size: 13px;
-}
-
-.lista-contas button{
-  background-color: tomato;
-  color: white;
-  cursor: pointer;
-  border: 0;
-  border-radius: 5px;
-  padding: 10px 0px;
-}
-</style>
